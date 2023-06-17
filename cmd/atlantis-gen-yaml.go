c290feda70c5056d28a204b5dae0b4cc83d60a91
AJ <aj@48k.io>
2023-06-17 15:49:56 +0100

    add version flag

--- cmd/atlantis-gen-yaml.go
+++ cmd/atlantis-gen-yaml.go
@@ -11,16 +11,26 @@ import (
 	"github.com/wakeful/atlantis-gen-yaml/pkg/template"
 )
 
-var targetPath string
+var (
+	targetPath  string
+	showVersion bool
+	version     = "dev"
+)
 
 func init() {
 	flag.StringVar(&targetPath, "path", ".", "PATH where to search for terragrunt.hcl files")
+	flag.BoolVar(&showVersion, "version", false, "show version")
 }
 
 const name = ".atlantis-conf.yaml"
 
 func main() {
 	flag.Parse()
+
+	if showVersion {
+		log.Println(version)
+		os.Exit(0)
+	}
 
 	searchPath, err := common.NormalisePath(targetPath)
 	if err != nil {
@@ -33,6 +43,7 @@ func main() {
 	}
 
 	var extraConfig string
+
 	if _, errReadConfFile := os.Stat(name); errReadConfFile == nil {
 		buff, _ := os.ReadFile(name)
 		extraConfig = string(buff)
